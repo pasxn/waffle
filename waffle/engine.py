0e84af92929698551232d83a8609772f44e96cc4
pasxn <pasansanjula.android@gmail.com>
2023-02-14 08:56:43 +0530

    added nn class to keep track of the network

---
+++ waffle/engine.py
@@ -0,0 +1,51 @@
+'''
+noop <- implement on all the backends
+neg
+relu
+exp
+sign
+add
+sub
+mul
+div
+pow
+cmpeq
+sum
+max
+
+conv <- implement on all the backends
+
+reshape <- implement on cpu only,prolly in somewhere like the shapetracker 
+permute
+slice
+expland
+flip
+
+Linear <- implement in nn file, use the ops implemented in all the backends
+Batchnorm2D
+MaxPool2D
+Conv2D
+
+sigmoid
+relu
+elu
+swish
+relu6
+hardswish
+tanh
+gelu
+leakyrelu
+mish
+softplus
+
+NonLeniarities <- prolly will be redundent
+
+There should be a flag to track the backend for all the backend functions
+Later some operations are explicitly mapped to the particular backend
+Some implementations will be redundent but the final optimized ops map will give the best results
+
+Numpy and GPU defa, if time permits, Neon
+
+When compiling the model shapetrecker will compile the kernels for all the sizes needed for the particular network and after that those binaries can be executed
+So there is no need of writing kernels with dynamic shapes, I think that is how it's done
+'''
--- waffle/nn.py
+++ waffle/nn.py
@@ -1,51 +1,9 @@
 '''
-noop <- implement on all the backends
-neg
-relu
-exp
-sign
-add
-sub
-mul
-div
-pow
-cmpeq
-sum
-max
-
-conv <- implement on all the backends
-
-reshape <- implement on cpu only,prolly in somewhere like the shapetracker 
-permute
-slice
-expland
-flip
-
-Linear <- implement in nn file, use the ops implemented in all the backends
-Batchnorm2D
-MaxPool2D
-Conv2D
-
-sigmoid
-relu
-elu
-swish
-relu6
-hardswish
-tanh
-gelu
-leakyrelu
-mish
-softplus
-
-NonLeniarities <- prolly will be redundent
-
-There should be a flag to track the backend for all the backend functions
-Later some operations are explicitly mapped to the particular backend
-Some implementations will be redundent but the final optimized ops map will give the best results
-
-Numpy and GPU defa, if time permits, Neon
-
-When compiling the model shapetrecker will compile the kernels for all the sizes needed for the particular network and after that those binaries can be executed
-So there is no need of writing kernels with dynamic shapes, I think that is how it's done
+onnx and compilation code will go here 
+extend class nn to keep track of kernels
+Layets will directly come from engine as functions
+This class will keep track of them using the layer constructs or the onnx graph  
 '''
+
+class nn:
+    pass
