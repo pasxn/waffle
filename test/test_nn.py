8eb18b5ff15dd85275a34fdbb21b3a64fdc0f937
pasxn <pasansanjula.android@gmail.com>
2023-03-01 20:04:18 +0530

    nn arithmetic  test

---
+++ test/test_nn.py
@@ -0,0 +1,32 @@
+import unittest
+
+from waffle.base import tensor
+from waffle.core import nn
+from waffle import ops
+
+class test_nn(unittest.TestCase):
+    
+  def test_nn_arithmetic(self):
+    
+    class nn_arith(nn):
+      def __init__(self):
+        super(nn_arith, self).__init__()
+        self.t1 = tensor.randn(5)
+        self.t2 = tensor.randn(4, 6)
+        self.t3 = tensor.randn(2, 3, 4, 1)
+
+      def forward(self):
+        x1 = ops.exp(self.t1 + self.t2)
+        x2 = self.t1 + x1
+        x3 = x2**1
+        x4 = ops.relu(self.t3 * x3.sum())
+        x5 = ops.log(x4.max())
+
+        return x5
+
+    n1 = nn_arith()
+    for i in range(100):
+      n1.forward()
+
+if __name__ == '__main__':
+  unittest.main()
