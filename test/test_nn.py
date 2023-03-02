8516a1ddcf4434a76bfb7329ffa507b5f98a8861
pasxn <pasansanjula.android@gmail.com>
2023-03-02 10:35:20 +0530

    random test with input

--- test/test_nn.py
+++ test/test_nn.py
@@ -7,16 +7,17 @@ from waffle import ops
 class test_nn(unittest.TestCase):
     
   def test_nn_arithmetic(self):
-    
+
     class nn_arith(nn):
-      def __init__(self):
+      def __init__(self, input):
         super(nn_arith, self).__init__()
+        self.t0 = input
         self.t1 = tensor.randn(5)
         self.t2 = tensor.randn(4, 6)
         self.t3 = tensor.randn(2, 3, 4, 1)
 
       def forward(self):
-        x1 = ops.exp(self.t1 + self.t2)
+        x1 = ops.exp(self.t1 + self.t2) + self.t0
         x2 = self.t1 + x1
         x3 = x2**1
         x4 = ops.relu(self.t3 * x3.sum())
@@ -24,7 +25,8 @@ class test_nn(unittest.TestCase):
 
         return x5
 
-    n1 = nn_arith()
+    t0 = tensor.randn(2, 3, 6)
+    n1 = nn_arith(t0)
     for i in range(100):
       n1.forward()
 
