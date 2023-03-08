a16a395d0e365ebdcb9e1e774d947b5ba70bf7d1
pasxn <pasansanjula.android@gmail.com>
2023-03-08 20:55:36 +0530

    lint

--- waffle/ops.py
+++ waffle/ops.py
@@ -19,49 +19,49 @@ def neg(x):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.neg(x)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.neg(x)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
 
 def relu(x):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.relu(x)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.relu(x)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
   
 def exp(x):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.exp(x)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.exp(x)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
 
 def log(x):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.log(x)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.log(x)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
 
 def add(x, y):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.add(x, y)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.add(x, y)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
 
 def sub(x, y):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.sub(x, y)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.sub(x, y)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
 
 def mul(x, y):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.mul(x, y)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.mul(x, y)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
 
 def div(x, y):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.div(x, y)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.div(x, y)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
 
 def pow(x, y):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.pow(x, y)
@@ -79,7 +79,7 @@ def sum(x, axis=None):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.sum(x, axis)
   elif GLOBAL_DEVICE == DEVICES.GPU: pass
   elif GLOBAL_DEVICE == DEVICES.HET: return cpu.sum(x, axis)
-  else:raise RuntimeError("device is not configured correctly") 
+  else:raise RuntimeError("device is not configured correctly")
 
 def max(x, axis=None):
   if   GLOBAL_DEVICE == DEVICES.CPU: return cpu.max(x, axis)
