47c9caabe4845d686c138dd8499011c7b38b367f
pasxn <pasansanjula.android@gmail.com>
2023-06-12 13:28:11 +0530

    implemented typing throughout the codebase

--- waffle/nn/layers.py
+++ waffle/nn/layers.py
@@ -4,17 +4,17 @@ from waffle import ops
 
 # ***** nn ops ****
 class Linear:
-  def __init__(self, in_features, out_features, bias=True):
+  def __init__(self, in_features: tensor, out_features:tensor, bias=True):
     self.weight = tensor.glorot_uniform(out_features, in_features)
     self.bias = tensor.zeros(out_features, 1) if bias else None
 
-  def set_weight(self, weight):
+  def set_weight(self, weight:tensor):
     self.weight = weight
 
-  def set_bias(self, bias):
+  def set_bias(self, bias:tensor):
     self.bias = bias
 
-  def __call__(self, x):
+  def __call__(self, x:tensor) -> tensor:
     x = self.weight@x
     return x.add(self.bias) if self.bias is not None else x
     
@@ -34,6 +34,6 @@ class MaxPool2D:
 
 # ***** nonLeniarities ****
 class Relu:
-  def __call__(self, x):
+  def __call__(self, x:tensor) -> tensor:
     return ops.relu(x)
   
--- waffle/tensor.py
+++ waffle/tensor.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-from typing import Tuple
+from typing import Tuple, Union, Callable
 
 from waffle import ops
 
@@ -22,42 +22,42 @@ class tensor:
 
   # ***** data handlers ****
   @property
-  def shape(self): return self.data.shape
+  def shape(self) -> Tuple[int, ...]: return self.data.shape
 
   @property
-  def len(self): return self.data.shape[0]
+  def len(self) -> int: return self.data.shape[0]
 
   @property
-  def dtype(self): return np.float32
+  def dtype(self) -> str: return str(np.float32).split("'")[1]
 
     
   # ***** creation helper functions *****
   @staticmethod
-  def zeros(*shape, **kwargs):
+  def zeros(*shape, **kwargs) -> 'tensor':
     return tensor(np.zeros(shape, dtype=np.float32), **kwargs)
 
   @staticmethod
-  def ones(*shape, **kwargs):
+  def ones(*shape, **kwargs) -> 'tensor':
     return tensor(np.ones(shape, dtype=np.float32), **kwargs)
 
   @staticmethod
-  def randn(*shape, **kwargs):
+  def randn(*shape, **kwargs) -> 'tensor':
     return tensor(np.random.randn(*shape).astype(np.float32), **kwargs)
     
   @staticmethod
-  def arange(stop, start=0, **kwargs):
+  def arange(stop:int, start=0, **kwargs) -> 'tensor':
     return tensor(np.arange(start=start, stop=stop).astype(np.float32), **kwargs)
 
   @staticmethod
-  def uniform(*shape, **kwargs):
+  def uniform(*shape, **kwargs) -> 'tensor':
     return tensor((np.random.uniform(-1., 1., size=shape)/np.sqrt(math.prod(shape))).astype(np.float32), **kwargs)
   
   @staticmethod
-  def glorot_uniform(*shape, **kwargs): 
+  def glorot_uniform(*shape, **kwargs) -> 'tensor': 
     return tensor.uniform(*shape, **kwargs).mul((6/(shape[0]+math.prod(shape[1:])))**0.5)
 
   @staticmethod
-  def eye(dim, **kwargs):
+  def eye(dim:int, **kwargs) -> 'tensor':
     return tensor(np.eye(dim).astype(np.float32), **kwargs)
   
 
@@ -65,10 +65,10 @@ class tensor:
   def resize(self, *shape, **kwargs): # order='C' or order='F'
     self.data = self.data.reshape(shape, **kwargs)
 
-  def reshape(self, *shape, **kwargs): # order='C' or order='F'
+  def reshape(self, *shape, **kwargs) -> 'tensor': # order='C' or order='F'
     return tensor(self.data.reshape(shape, **kwargs))
     
-  def concat(self, y, axis=0, order=0):
+  def concat(self, y:'tensor', axis=0, order=0) -> 'tensor':
     if not isinstance(y, tensor): raise RuntimeError("input must be a waffle tensor")
     if order == 0:
       return tensor(np.concatenate((self.data, y.data), axis=axis))
@@ -77,7 +77,7 @@ class tensor:
     else:
       raise RuntimeError(f"order is goven as {order}. order must be 0 or 1")
         
-  def pad2d(self, arg, mode='constant'):
+  def pad2d(self, arg:Union[Tuple[int, ...], int], mode='constant') -> 'tensor':
     if isinstance(arg, tuple):
       return tensor(np.pad(self.data, arg, mode=mode))
     elif isinstance(arg, int):
@@ -85,30 +85,30 @@ class tensor:
     else:
       raise RuntimeError("argument must be an int ora tuple ((top, bottom), (left, right))")
         
-  def transpose(self):
+  def transpose(self) -> 'tensor':
     return tensor(self.data.transpose())
 
-  def flatten(self):
+  def flatten(self) -> 'tensor':
     return tensor(self.data.flatten())
     
   def reval(self):
     self.data = self.data.flatten()
 
-  def premute(self, dim:Tuple[int, ...]):
+  def premute(self, dim:Tuple[int, ...]) -> 'tensor':
     # dim (1, 0, 2): numbers refer to the dimensions of the original array assuming it's a 3 dimensional array
     return tensor(self.data.transpose(dim))
 
-  def slice(self, start:Tuple[int, ...], end:Tuple[int, ...]):
+  def slice(self, start:Tuple[int, ...], end:Tuple[int, ...]) -> 'tensor':
     # ex: a.slice((1, 2), (3, 3))
     return tensor(self.data[start[0]:start[1], end[0]:end[1]])
     
-  def expand(self, axis=None):
+  def expand(self, axis=None) -> 'tensor':
     if axis is None or isinstance(axis, tuple) or isinstance(axis, int):
       return tensor(np.expand_dims(self.data, axis=axis))
     else:
       raise RuntimeError("axis must be an int or a tuple")
     
-  def flip(self, axis=None):
+  def flip(self, axis=None) -> 'tensor':
     if axis is None or isinstance(axis, tuple) or isinstance(axis, int):
       return tensor(np.flip(self.data, axis=axis))
     else:
@@ -116,13 +116,13 @@ class tensor:
     
   
   # ***** slicing and indexing *****
-  def __getitem__(self, val):
+  def __getitem__(self, val:int) -> 'tensor':
     return tensor(self.data[val])
   
 
   # ***** broadcasting mechanism *****
   @staticmethod
-  def broadcasted(fxn, tx, ty):
+  def broadcasted(fxn:Callable[['tensor'], 'tensor'], tx:'tensor', ty:'tensor') -> 'tensor':
     if isinstance(tx, int) or isinstance(tx, float): tx = tensor([tx])
     elif isinstance(ty, int) or isinstance(ty, float): ty = tensor([ty])
 
@@ -140,34 +140,34 @@ class tensor:
   
 
   # ***** arithmetic operations*****
-  def __neg__(self): return ops.neg(self)
+  def __neg__(self) -> 'tensor': return ops.neg(self)
 
-  def add(self, y): return self.broadcasted(ops.add, self, y)
-  def sub(self, y): return self.broadcasted(ops.sub, self, y)
-  def mul(self, y): return self.broadcasted(ops.mul, self, y)
-  def div(self, y): return self.broadcasted(ops.div, self, y)
-  def pow(self, y): return self.broadcasted(ops.pow, self, y)
-  def dot(self, y): return ops.gemm(self, y)
-  def sum(self, axis=None): return ops.sum(self, axis)
-  def max(self, axis=None): return ops.max(self, axis)
+  def add(self, y:'tensor') -> 'tensor': return self.broadcasted(ops.add, self, y)
+  def sub(self, y:'tensor') -> 'tensor': return self.broadcasted(ops.sub, self, y)
+  def mul(self, y:'tensor') -> 'tensor': return self.broadcasted(ops.mul, self, y)
+  def div(self, y:'tensor') -> 'tensor': return self.broadcasted(ops.div, self, y)
+  def pow(self, y:'tensor') -> 'tensor': return self.broadcasted(ops.pow, self, y)
+  def dot(self, y:'tensor') -> 'tensor': return ops.gemm(self, y)
+  def sum(self, axis=None) -> 'tensor': return ops.sum(self, axis)
+  def max(self, axis=None) -> 'tensor': return ops.max(self, axis)
 
-  def __add__(self, y): return self.add(y)
-  def __sub__(self, y): return self.sub(y)
-  def __mul__(self, y): return self.mul(y)
-  def __pow__(self, y): return self.pow(y)
-  def __truediv__(self, y): return self.div(y)
-  def __matmul__(self, x): return self.dot(x)
+  def __add__(self, y:'tensor') -> 'tensor': return self.add(y)
+  def __sub__(self, y:'tensor') -> 'tensor': return self.sub(y)
+  def __mul__(self, y:'tensor') -> 'tensor': return self.mul(y)
+  def __pow__(self, y:'tensor') -> 'tensor': return self.pow(y)
+  def __truediv__(self, y:'tensor') -> 'tensor': return self.div(y)
+  def __matmul__(self, x:'tensor') -> 'tensor': return self.dot(x)
 
-  def __radd__(self, y): return self.add(y)
-  def __rsub__(self, y): return self.sub(y)
-  def __rmul__(self, y): return self.mul(y)
-  def __rpow__(self, y): return self.pow(y)
-  def __rtruediv__(self, y): return self.div(y)
-  def __rmatmul__(self, x): return self.dot(x)
+  def __radd__(self, y:'tensor') -> 'tensor': return self.add(y)
+  def __rsub__(self, y:'tensor') -> 'tensor': return self.sub(y)
+  def __rmul__(self, y:'tensor') -> 'tensor': return self.mul(y)
+  def __rpow__(self, y:'tensor') -> 'tensor': return self.pow(y)
+  def __rtruediv__(self, y:'tensor') -> 'tensor': return self.div(y)
+  def __rmatmul__(self, x:'tensor') -> 'tensor': return self.dot(x)
 
-  def __iadd__(self, y): return self.add(y)
-  def __isub__(self, y): return self.sub(y)
-  def __imul__(self, y): return self.mul(y)
-  def __ipow__(self, y): return self.pow(y)
-  def __itruediv__(self, y): return self.div(y)
-  def __imatmul__(self, x): return self.dot(x)
+  def __iadd__(self, y:'tensor') -> 'tensor': return self.add(y)
+  def __isub__(self, y:'tensor') -> 'tensor': return self.sub(y)
+  def __imul__(self, y:'tensor') -> 'tensor': return self.mul(y)
+  def __ipow__(self, y:'tensor') -> 'tensor': return self.pow(y)
+  def __itruediv__(self, y:'tensor') -> 'tensor': return self.div(y)
+  def __imatmul__(self, x:'tensor') -> 'tensor': return self.dot(x)
