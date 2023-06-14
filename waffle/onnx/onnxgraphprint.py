6ca12db6dfd770e1d0b669436a4594b48cef847f
kayy4 <kavinamantha8@gmail.com>
2023-06-15 00:50:36 +0530

    Added Print Weights & Bias in node details

--- waffle/onnx/onnxgraphprint.py
+++ waffle/onnx/onnxgraphprint.py
@@ -76,50 +76,7 @@ for initializer in initializer_list:
     print("Weight Shape:", weight_shape)
     print("Weight Values:", weight_array)
 
-#%% For Node Details
-import onnx
-import numpy as np
-
-# Load the ONNX model
-model_path = '../../models/resnet18/resnet18.onnx'
-model = onnx.load(model_path)
-
-# Print node details
-print("Node Details:")
-for i, node in enumerate(model.graph.node):
-    print(f"========================================== Node {i}:==========================================")
-    print("Name:", node.name)
-    print()
-    print("Input Nodes:", node.input)
-    print()
-    print("Output Nodes:", node.output)
-    print()
-    print("Op Type:", node.op_type)
-    print()
-    print("Attribute:")
-    for attr in node.attribute:
-        print(f"- Name: {attr.name}")
-        print(f"  Type: {attr.type}")
-        #attributes can be numerics (kernal size,shape,etc.)
-        if attr.type == onnx.AttributeProto.FLOATS:
-            print(f"  Values1: {attr.floats}")
-        elif attr.type == onnx.AttributeProto.INTS:
-            print(f"  Values2: {attr.ints}")
-        #attributes can be stored in texts (Names)
-        elif attr.type == onnx.AttributeProto.STRING:
-            print(f"  Values3: {attr.s}")
-        elif attr.type == onnx.AttributeProto.TENSOR:
-            print(f" Tensor Value: ", attr.t.float_data)
-        print()
-        print()
-    
-    #Print Weights Also
-
-    print()
-    print()
-
-
-# %%----------------------------------------------------------------------------
+#%%# %%----------------------------------------------------------------------------
 #for input and Output Details
 
 # Print input details
@@ -159,5 +116,70 @@ for initializer in model.graph.initializer:
 
 print("Weight Tensor Shape:", weight_tensor.dims)
 print("Bias Tensor Shape:", bias_tensor.dims)
+
+
+
+
+
+
+#%% For Node Details including bias & Weights
+import onnx
+import numpy as np
+
+# Load the ONNX model
+model_path = '../../models/resnet18/resnet18.onnx'
+model = onnx.load(model_path)
+
+initializer_list = model.graph.initializer
+
+# Print node details
+print("Node Details:")
+for i, node in enumerate(model.graph.node):
+    print(f"========================================== Node {i}:==========================================")
+    print("Name:", node.name)
+    print()
+    print("Input Nodes:", node.input)
+    print()
+    print("Output Nodes:", node.output)
+    print()
+    print("Op Type:", node.op_type)
+    print()
+    print("Attribute:")
+    for attr in node.attribute:
+        print(f"- Name: {attr.name}")
+        print(f"  Type: {attr.type}")
+        #attributes can be numerics (kernal size,shape,etc.)
+        if attr.type == onnx.AttributeProto.FLOATS:
+            print(f"  Values1: {attr.floats}")
+        elif attr.type == onnx.AttributeProto.INTS:
+            print(f"  Values2: {attr.ints}")
+        #attributes can be stored in texts (Names)
+        elif attr.type == onnx.AttributeProto.STRING:
+            print(f"  Values3: {attr.s}")
+        elif attr.type == onnx.AttributeProto.TENSOR:
+            print(f" Tensor Value: ", attr.t.float_data)
+        print()
+        print()
+    
+    #Print Weights Also
+    #for initializer in initializer_list:
+        # Access the name, shape, and values of the weight tensor
+        weight_name = initializer.name
+        weight_shape = initializer.dims
+        weight_values = initializer.raw_data
+
+        #raw_data to a NumPy array
+        import numpy as np
+        weight_array = np.frombuffer(weight_values, dtype=np.float32).reshape(weight_shape)
+
+        # Print the weight information
+        print("Weight Name:", weight_name)
+        print()
+        print("Weight Shape:", weight_shape)
+        print()
+        #print("Weight Values:", weight_array)
+
+print()
+print()
 
 # %%
