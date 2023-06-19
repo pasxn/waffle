b89de3d3576131658eb1faf6ea962e90b128fe51
pasxn <pasansanjula.android@gmail.com>
2023-06-19 14:27:16 +0530

    demo halfway plus fixing tensor.arrange

---
+++ extra/demo/abstractions.py
@@ -0,0 +1,74 @@
+# %%
+
+# importing our package and other helper packages for the demo
+from waffle import tensor
+import numpy as np
+
+# %%
+
+# creating tensors from
+
+# an integer
+a = tensor(1)
+
+# a float
+b = tensor(2.4)
+
+# a python list
+c = tensor([1, 2, 3, 4, 5])
+
+# a numpy array
+d = tensor(np.array([[1, 2], [1, 2], [1, 2]]))
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# %%
+
+# obtaining the peoperties of the tensors using our framework
+print(f"shape: {d.shape}")
+print(f"length: {d.len}")
+print(f"data type: {d.dtype}")
+
+# %%
+
+# creating tensors of a given shape of given properties
+
+# zeros
+a = tensor.zeros(2, 2)
+
+# ones
+b = tensor.ones(3, 3, 3)
+
+# random floating point numbers
+c = tensor.randn(8)
+
+# evenly spaced values within a given interval
+d = tensor.arange(0, 5, 0.5)
+
+
+print(f"a [zeros]: {a.data}\n")
+print(f"b [ones]: {b.data}\n")
+print(f"c [random floating point numbers]: {c.data}\n")
+print(f"c [evenly spaced values within a given interval]: {d.data}\n")
+
+# %%
+
+# unifrom distribution of numbers within-1 and 1 in a given shape
+e = tensor.uniform(4, 4)
+
+# glorot unifrom distribution of numbers within-1 and 1 in a given shape
+f = tensor.glorot_uniform(4, 4)
+
+# idenitiy matrix of s given size
+g = tensor.eye(10)
+
+print(f"e [unifrom distribution of numbers within-1 and 1 in a given shape]: {e.data}\n")
+print(f"f [glorot unifrom distribution of numbers within-1 and 1 in a given shape]: {f.data}\n")
+print(f"g [idenitiy matrix of s given size]: {g.data}")
+
+# %%
+
+
