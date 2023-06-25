bb781d1784f27404b8b9152be126b2ac3f82b811
CodewithAfkar <afkar.net11@gmail.com>
2023-06-25 14:38:52 +0100

    Added all the initial kernels

--- waffle/backend/gpu_backend/kernels/mul_1.cpp
+++ waffle/backend/gpu_backend/kernels/mul_1.cpp
@@ -3,85 +3,83 @@
 #include "V3DLib.h"
 #include <CmdParameters.h>
 #include "Support/Settings.h"
-//to add
+
 using namespace V3DLib;
 
 V3DLib::Settings settings;
 
-void add(Int n, Int::Ptr x, Int::Ptr y, Int::Ptr z) {
+void mul(Int n, Int::Ptr x, Int::Ptr y, Int::Ptr z) {
   For (Int i = 0, i<n, i+=16)
     Int a = x[i];
     Int b = y[i];
-    z[i] = a + b;
+    z[i] = a * b;
   End
 }
 
-void addArrays(int size, const int* array1, const int* array2, int* result) {
+void mulArrays(int size, const int* array1, const int* array2, int* result) {
     for (int i = 0; i < size; i++) {
     	int ca = array1[i];
     	int cb = array2[i];
-    	result[i] = ca + cb;
+    	result[i] = ca * cb;
     }
 }
-
 
 int main(int argc, const char *argv[]) {
   
   int size = 400000;
   int iterations = 1000;
-
-  // GPU arrays
+//GPU arrays
   Int::Array a(size);
   Int::Array b(size);
   Int::Array r(size);
-
   for (int i = 0; i < size; i++) {
     a[i] = 1;
-    b[i] = 1;
+    b[i] = 2;
   }
-
-  // CPU arrays
+ 
+ //CPU arrays 
   int aa[size];
   int bb[size];
   int rr[size];
-
-  for(int i = 0; i < size; i++) {
+  for(int i=0; i<size; i++){
     aa[i]=1;
-    bb[i]=1;
+    bb[i]=2;
   }
-
-  // GPU ececution
+  
+  //GPU execution
   settings.init(argc, argv);
-  auto k = compile(add);
-
-  auto start_gpu = std::chrono::high_resolution_clock::now();
-
-  for(int y = 0; y < iterations ; y++) {
-    k.load(size, &a, &b, &r);
+  auto k = compile(mul);
+  
+  auto start = std::chrono::high_resolution_clock::now();
+  for(int y=0; y<iterations ;y++){            
+    k.load(size, &a, &b, &r);  
   }
   settings.process(k);
-  auto end_gpu = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> duration_gpu = end_gpu - start_gpu;
-
-  // CPU ececution
-  auto start_cpu = std::chrono::high_resolution_clock::now();
-  for(int y = 0; y < iterations ; y++) {
-    addArrays(size, aa, bb,rr);
+  auto end = std::chrono::high_resolution_clock::now(); //time ends here 
+  
+  std::chrono::duration<double> duration = end - start;
+  
+  //CPU execution
+  auto start_cpu = std::chrono::high_resolution_clock::now();  //time start  
+  for(int y=0; y<iterations; y++){
+    mulArrays(size, aa, bb,rr);
   }
-  auto end_cpu = std::chrono::high_resolution_clock::now();
+  auto end_cpu = std::chrono::high_resolution_clock::now(); //time end 
+  
   std::chrono::duration<double> duration_cpu = end_cpu - start_cpu;
-
-
-
+  
   // functional verification
   for(int j = 0; j < size; j++) {
     if(r[j] != rr[j])
       printf("CPU output and GPU output is not equal at j = %d \n", j);
   }  
-
-  // time log
+  
+  
+  //time log
   printf(".........Execution Time.........\n");
   printf("Execution time for CPU: %f seconds\n", duration_cpu.count());
-  printf("Execution time for GPU: %f seconds\n", duration_gpu.count());
+  printf("Execution time for GPU: %f seconds\n", duration.count());
+  
+  
+  return 0;
 }
-
