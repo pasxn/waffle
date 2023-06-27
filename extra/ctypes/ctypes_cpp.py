23123afc9c44cdab510f7e6b4d072c8d87d4250e
pasxn <pasansanjula.android@gmail.com>
2023-06-27 13:57:43 +0530

    indentation

--- extra/ctypes/ctypes_cpp.py
+++ extra/ctypes/ctypes_cpp.py
@@ -15,18 +15,18 @@ add_lib.add.argtypes = (ctypes.c_int, ctypes.POINTER(ctypes.c_float),
 add_lib.add.restype = None
 
 def add(size, x, y):
-    # Convert Python lists to C arrays
-    x_arr = (ctypes.c_float * size)(*x)
-    y_arr = (ctypes.c_float * size)(*y)
-    z_arr = (ctypes.c_float * size)()
+  # Convert Python lists to C arrays
+  x_arr = (ctypes.c_float * size)(*x)
+  y_arr = (ctypes.c_float * size)(*y)
+  z_arr = (ctypes.c_float * size)()
 
-    # Call the C function
-    add_lib.add(ctypes.c_int(size), x_arr, y_arr, z_arr)
+  # Call the C function
+  add_lib.add(ctypes.c_int(size), x_arr, y_arr, z_arr)
 
-    # Convert the C array back to a Python list
-    z = np.asarray(list(z_arr), dtype=np.float32)
+  # Convert the C array back to a Python list
+  z = np.asarray(list(z_arr), dtype=np.float32)
 
-    return z
+  return z
 
 # Example usage
 a = np.asarray([i for i in range(5)], dtype=np.float32)
