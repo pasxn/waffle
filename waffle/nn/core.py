27890e20e3e319dcd227caa15d8729396b588076
pasxn <pasansanjula.android@gmail.com>
2023-06-30 00:16:52 +0530

    load, and compile feature

--- waffle/nn/core.py
+++ waffle/nn/core.py
@@ -1,12 +1,18 @@
 from waffle import tensor
+from waffle import ops
 
 
 class Module:
   def __init__(self, *args, **kwargs):
     pass
 
-  def load_onnx(self):
+  def load(self):
     pass
+    # get the linearized shit as a list of dicts
+
+  def compile(self):
+    ops.compile()
+    # create the in memory graph
 
   def run(self, x:tensor) -> tensor:
     return self.forward(x)
