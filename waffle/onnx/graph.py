74df15999f55a29266ad186193ebaae6d820bcbd
pasxn <pasansanjula.android@gmail.com>
2023-06-30 20:33:52 +0530

    arch definition

--- waffle/onnx/graph.py
+++ waffle/onnx/graph.py
@@ -4,6 +4,12 @@ class Node:
   def __init__(self):
     self.node_name = None
     self.callable = None
+    self.weights = None
+    self.biases = None
+
+  def load_callable(self):
+    callable.set_weights(self.weights)
+    callable.set_weights(self.biases)
 
 class Graph:
   def __init__(self, graph):
