3b3e4fa20d5aff4aa090c4344322ce207af3448f
pasxn <pasansanjula.android@gmail.com>
2023-07-08 17:09:55 +0530

    ugh

--- extra/dlfcn/dlfcn_ctypes/add.cpp
+++ extra/dlfcn/dlfcn_ctypes/add.cpp
@@ -1,12 +1,11 @@
-// http://man7.org/linux/man-pages/man3/dlopen.3.html
+//http://man7.org/linux/man-pages/man3/dlopen.3.html
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dlfcn.h>
-#include <gnu/lib-names.h>
 
-void add_run(float num, float* result) {
+float* add_run(float num) {
   void *handle;
   float* (*plus_one)(float);
   char *error;
@@ -17,22 +16,26 @@ void add_run(float num, float* result) {
 
   plus_one = (float* (*)(float)) dlsym(handle, "plus_one");
 
-  result = (*plus_one)(num);
+  float* result = (*plus_one)(num);
   dlclose(handle);
 
   printf("%f\n", *result);
+
+  return result;
 }
 
 extern "C" {
-  void run(float num, float* result) {
-    add_run(num, result);
+  void run(float num) {
+    add_run(num);
   }
 }
 
 int main() {
-  float* zz = new float();
+  float* z = (float*)malloc(sizeof(float));
   
-  add_run(1.0f, new float());
+  z = add_run(1.0f);
 
-  //printf("%f\n", *z);
+  printf("%f\n", *z);
+
+  free(z);
 }
