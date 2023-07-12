13bae81ac09bf59c75334187e94fd69e4bfbb03b
Pasan Perera <pasansanjula.android@gmail.com>
2023-07-12 11:57:09 +0530

    VC4 assembler (#61)
    * init pyvideocore test
    
    * assembler done
    
    * single reg assembler test
    
    * single reg assembler quick fix
    
    * ugh test
    
    * fixing
    
    * pytest is a headache sometimes
    
    * assembler is hard

---
+++ extra/pyvc4_test/basic.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from videocore.assembler import qpu
+from videocore.driver import Driver
+
+@qpu
+def add(asm):
+  # Load two vectors of length 16 from the host memory (address=uniforms[0]) to VPM
+  setup_dma_load(nrows=2)
+  start_dma_load(uniform)
+  wait_dma_load()
+
+  # Setup VPM read/write operations
+  setup_vpm_read(nrows=2)
+  setup_vpm_write()
+
+  # Compute a + b
+  mov(r0, vpm)
+  mov(r1, vpm)
+  fadd(vpm, r0, r1)
+
+  # Store the result vector from VPM to the host memory (address=uniforms[1])
+  setup_dma_store(nrows=1)
+  start_dma_store(uniform)
+  wait_dma_store()
+
+  # Finish the thread
+  exit()
+
+with Driver() as drv:
+  # Input vectors
+  a = np.random.random(16).astype('float32')
+  b = np.random.random(16).astype('float32')
+
+  # Copy vectors to shared memory for DMA transfer
+  inp = drv.copy(np.r_[a, b])
+  out = drv.alloc(16, 'float32')
+
+  # Run the program
+  drv.execute(n_threads=1, program=drv.program(add), uniforms=[inp.address, out.address])
+
+  print(' a '.center(80, '='))
+  print(a)
+  print(' b '.center(80, '='))
+  print(b)
+  print(' a + b '.center(80, '='))
+  print(out)
+  print(f'\nmean error: {np.abs(a + b - out).sum()/len(out)}\n')
