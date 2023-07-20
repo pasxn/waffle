49fef5a6918e620fd8846dcc88fe89a69ac77871
pasxn <pasansanjula.android@gmail.com>
2023-07-20 18:41:01 +0530

    image and filter dimention cleanup

--- extra/gemm_conv/gemm_conv_2d.py
+++ extra/gemm_conv/gemm_conv_2d.py
@@ -5,28 +5,34 @@ image = np.array([i for i in range(1, 49)]).astype(np.float32).reshape(3, 4, 4)
 
 filtr = np.array([i for i in range(1, 25)]).astype(np.float32).reshape(2, 3, 2, 2)
 
+num_channels = image.shape[0]
+image_height = image.shape[1]
+image_width  = image.shape[2]
+
+# a filter has x kernels of y channels
+num_kernels   = filtr.shape[0]
+num_channels  = filtr.shape[1]
+filter_height = filtr.shape[2]
+filter_width  = filtr.shape[3]
+
 reshaped_x = []
-for h in range(image.shape[0]):
+for h in range(num_channels):
   filter_out = []
-  for i in range(image.shape[1]-filtr.shape[2]+1):
-    for j in range(image.shape[2]-filtr.shape[3]+1):
-      filter_out.append(image[h][i:i+filtr.shape[2], j:j+filtr.shape[3]].flatten().tolist())
+  for i in range(image_height-filter_height+1):
+    for j in range(image_width-filter_width+1):
+      filter_out.append(image[h][i:i+filter_height, j:j+filter_width].flatten())
 
   filter_out = np.array(filter_out).transpose()
   reshaped_x.append(filter_out)
 
-reshaped_x = np.array(reshaped_x).reshape(reshaped_x[0].shape[0]*image.shape[0], reshaped_x[0].shape[1])
-
-num_kernels = len(filtr)
+reshaped_x = np.array(reshaped_x).reshape(reshaped_x[0].shape[0]*num_channels, reshaped_x[0].shape[1])
 
 reshaped_w = filtr.reshape(num_kernels, reshaped_x.shape[0])
 
-print(reshaped_x.shape, reshaped_w.shape)
-
 conv_result = reshaped_w@reshaped_x
 
-height = int(((image.shape[1] - filtr.shape[2] + 2*(0))/1) + 1) # 0: number of padding, 1: stride
-width = int(((image.shape[2] - filtr.shape[3] + 2*(0))/1) + 1) # 0: number of padding, 1: stride
+height = int(((image_height - filter_height + 2*(0))/1) + 1)  # 0: number of padding, 1: stride
+width  = int(((image_width - filter_width + 2*(0))/1) + 1)    # 0: number of padding, 1: stride
 
 conv_result = conv_result.reshape(num_kernels, height, width)
 
