3fd413bc8324518be6338faa2343618a82dce830
pasxn <pasansanjula.android@gmail.com>
2023-07-25 00:00:03 +0530

    issue in onnxread.py

--- waffle/onnx/onnxread.py
+++ waffle/onnx/onnxread.py
@@ -3,47 +3,47 @@ from waffle.onnx.graph import Node
 import onnx
 import numpy as np
 
-def read_onnx(model_path):
-    model = onnx.load(model_path); nodes = []
+def read_onnx(model_path:str):
+  model = onnx.load(model_path); nodes = []
 
-    for i, node in enumerate(model.graph.node):
-        attributes = []
-        for attr in node.attribute:
-            attribute = {}
-            attribute['name'] = attr.name
-            attribute['type'] = attr.type
+  for i, node in enumerate(model.graph.node):
+    attributes = []
+    for attr in node.attribute:
+      attribute = {}
+      attribute['name'] = attr.name
+      attribute['type'] = attr.type
 
-            if attr.type == onnx.AttributeProto.FLOATS:
-                attribute['values'] = list(attr.floats)
-            elif attr.type == onnx.AttributeProto.INTS:
-                attribute['values'] = list(attr.ints)
-            elif attr.type == onnx.AttributeProto.STRING:
-                attribute['values'] = attr.s.decode('utf-8')
-            elif attr.type == onnx.AttributeProto.TENSOR:
-                tensor_data = np.frombuffer(attr.t.raw_data, dtype=np.float32)
-                attribute['values'] = tensor_data.reshape(attr.t.dims)
+      if attr.type == onnx.AttributeProto.FLOATS:
+        attribute['values'] = list(attr.floats)
+      elif attr.type == onnx.AttributeProto.INTS:
+        attribute['values'] = list(attr.ints)
+      elif attr.type == onnx.AttributeProto.STRING:
+        attribute['values'] = attr.s.decode('utf-8')
+      elif attr.type == onnx.AttributeProto.TENSOR:
+        tensor_data = np.frombuffer(attr.t.raw_data, dtype=np.float32)
+        attribute['values'] = tensor_data.reshape(attr.t.dims)
 
-            attributes.append(attribute)
+      attributes.append(attribute)
 
-        weight_tensors = []
-        for input_name in node.input:
-            for initializer in model.graph.initializer:
-                if input_name == initializer.name:
-                    weight_tensor = {}
-                    weight_tensor['name'] = initializer.name
-                    weight_tensor['shape'] = initializer.dims
+    weight_tensors = []
+    for input_name in node.input:
+      for initializer in model.graph.initializer:
+        if input_name == initializer.name:
+          weight_tensor = {}
+          weight_tensor['name'] = initializer.name
+          weight_tensor['shape'] = initializer.dims
 
-                    weight_array = np.frombuffer(initializer.raw_data, dtype=np.float32)
-                    weight_tensor['values'] = weight_array.reshape(initializer.dims)
+          weight_array = np.frombuffer(initializer.raw_data, dtype=np.float32)
+          weight_tensor['values'] = weight_array.reshape(initializer.dims)
 
-                    weight_tensors.append(weight_tensor)
+          weight_tensors.append(weight_tensor)
 
-        node_weight = None; node_bias = None
-        assert len(weight_tensors) <= 2, 'there are more tan 2 weight tensors!'
-        for i in weight_tensors:
-            node_weight = i if i['name'] == 'weight' else None
-            node_bias   = i if i['name'] == 'bias' else None
+    node_weight = None; node_bias = None
+    assert len(weight_tensors) <= 2, 'there are more tan 2 weight tensors!'
+    for i in weight_tensors:
+      node_weight = i if i['name'] == 'weight' else None
+      node_bias   = i if i['name'] == 'bias' else None
 
-        nodes.append(Node(node.name, node.input, node.output, node.op_type, attributes, node_weight, node_bias))
+    nodes.append(Node(node.name, node.input, node.output, node.op_type, attributes, node_weight, node_bias))
 
     return nodes
--- waffle/onnx/test.py
+++ waffle/onnx/test.py
@@ -1,6 +1,6 @@
 from waffle.onnx.onnxread import read_onnx
 
-model_path = 'models/resnet18/resnet18.onnx'
+model_path = 'models/resnet50/resnet50.onnx'
 graph_nodes = read_onnx(model_path)
 
 print(graph_nodes)
