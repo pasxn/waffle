0ad6ff225056996afe0480f3a410512cab4b273e
pasxn <pasansanjula.android@gmail.com>
2023-07-26 21:39:49 +0530

    multiple inputs

--- waffle/onnx/onnxlist.py
+++ waffle/onnx/onnxlist.py
@@ -9,12 +9,14 @@ class onnxList:
 
   def hard_traverse(self):
     for node in self.linearized_list:
+      inputs = []
       for input in node.input:
         if input == 'input.1':
-          node.traverse_input = -1
+          inputs.append(-1)
         for i in range(len(self.linearized_list)):
           if input in self.linearized_list[i].output:
-            node.traverse_input = i
+            inputs.append(i)
+        node.traverse_input = inputs
 
     for i in range(len(self.linearized_list)):
       print(i, ':', self.linearized_list[i].name ,">>>", self.linearized_list[i].traverse_input)
