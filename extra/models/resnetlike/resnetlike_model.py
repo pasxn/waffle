03c4b35035d7d162047e2e53d7459fddf210fdc1
kayy4 <98510066+kayy4@users.noreply.github.com>
2023-08-07 12:05:21 +0530

    resnetlike model implementation  (#78)
    * resnet like model architecture
    
    * resnet like modedl
    
    * Remove resnetlike.ckpt
    
    * added maxpool to te model
    
    * fixed name issue in .ckpt

---
+++ extra/models/resnetlike/resnetlike_model.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+import torch.optim as optim
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        
+        # If the input channels and output channels differ, add a 1x1 convolution to match dimensions.
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.shortcut = nn.Sequential()
+
+    def forward(self, x):
+        residual = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = F.max_pool2d(x, 1)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x += self.shortcut(residual)
+        x = self.relu(x)
+        return x
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.residual_block1 = ResidualBlock(32, 64, stride=2)
+        self.residual_block2 = ResidualBlock(64, 64)
+        self.fc = nn.Linear(64 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.residual_block1(x)
+        x = self.residual_block2(x)
+        x = F.max_pool2d(x, 2) 
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+    
---
+++ extra/models/resnetlike/resnetlike_train.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+import torch.optim as optim
+
+from extra.models.resnetlike.resnetlike_model import ResNet
+from extra.models.resnetlike.resnetlike_util import train_loader, test_loader 
+
+# Initialize the ResNet model
+model = ResNet()
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+num_epochs = 1
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    print(f"Epoch {epoch+1}, Loss: {running_loss / len(train_loader)}")
+
+# Evaluation
+model.eval()
+correct = 0
+total = 0
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Test Accuracy: {100 * correct / total:.2f}%")
+
+#save the model  Checkpoint
+torch.save(model.state_dict(), 'extra/models/resnetlike/resnetlike_mnist.ckpt')
+print("Model successfully saved in CKPT format!")
+
+# Save the model in ONNX format
+dummy_input = torch.randn(1, 1, 28, 28).to(device)
+input_names = ['input']
+output_names = ['output']
+onnx_path = 'extra/models/resnetlike/resnetlike_mnist.onnx'
+torch.onnx.export(model, dummy_input, onnx_path, input_names=input_names, output_names=output_names)
+print("Model successfully saved in ONNX format!")
