3048ea18b11e2492669f79861a6f7e5c427481bc
kayy4 <kavinamantha8@gmail.com>
2023-09-23 00:47:26 +0530

    Traversal is still in prog

--- waffle/onnx/onnxgraph.py
+++ waffle/onnx/onnxgraph.py
@@ -4,7 +4,7 @@ from waffle.onnx.node import Node
 import os
 from collections import deque
 WFLDBG = os.environ.get('WFLDBG')
-#WFLDBG = 1
+WFLDBG = 1
 class onnxGraph:
     def __init__(self, linearized_list: List[Node]):
         self.linearized_list = linearized_list
@@ -32,4 +32,41 @@ class onnxGraph:
 
 
     def run(self, input: tensor) -> tensor:
-      pass
+            self.input_tensor = input
+            visited = {node: False for node in self.graph.keys()}
+
+            def dfs(node):
+                visited[node] = True
+
+                # Recursively visit connected nodes
+                for connected_node in self.graph[node]:
+                    if not visited[connected_node]:
+                        dfs(connected_node)
+
+                # Compute the output of the current node using its compute_node method
+                if node.traverse_input is None:
+                    node.compute_node(self.input_tensor)
+                elif len(node.traverse_input) == 1:
+                    input_node = self.graph[node][0]
+                    node.compute_node(input_node.output_computed)
+                elif len(node.traverse_input) == 2:
+                    input_node1, input_node2 = self.graph[node]
+                    node.compute_node(input_node1.output_computed, input_node2.output_computed)
+                elif len(node.traverse_input) == 3:
+                    input_node1, input_node2, input_node3 = self.graph[node]
+                    node.compute_node(input_node1.output_computed, input_node2.output_computed, input_node3.output_computed)
+
+            # Find the starting nodes (nodes with no incoming connections)
+            start_nodes = [node for node, connected_nodes in self.graph.items() if not any(connected_node in self.graph for connected_node in connected_nodes)]
+
+            for start_node in start_nodes:
+                if not visited[start_node]:
+                    dfs(start_node)
+
+            result = self.linearized_list[-1].output_computed
+
+            # Reset output_computed for all nodes
+            for node in self.linearized_list:
+                node.output_computed = None
+
+            return result
